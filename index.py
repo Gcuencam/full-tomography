e4eed829a9cb17efbefb5e4d3c8f7bf74ce394cb
Gabriel Cuenca <gcuencamoncin@gmail.com>
2022-02-17 13:30:17 +0100

    :sparkles: Added buildWState to build a generalization of a W state

--- index.py
+++ index.py
@@ -5,41 +5,37 @@ from qiskit.providers.aer import QasmSimulator
 from qiskit.visualization import plot_histogram
 from qiskit.quantum_info import Statevector
 
-def main(): 
+from src.lib.wState import buildWState
+
+
+def main():
+
     # Use Aer's qasm_simulator
     simulator = QasmSimulator()
 
     # Create a Quantum Circuit acting on the q register
-    circuit = QuantumCircuit(3,3)
-
-    circuit.ry(2*np.arccos(1/m.sqrt(3)), 0)
-    circuit.ch(0, 1)
-    circuit.cx(1, 2)
-    circuit.cx(0, 1)
-    circuit.x(0)
+    q = 5
+    circuit = QuantumCircuit(q, q)
+    circuit = buildWState(circuit, 0, 2)
 
     psi = Statevector.from_instruction(circuit)
-    # Probabilities for measuring both qubits
+    # Probabilities for measuring qubits
     probs = psi.probabilities_dict()
     print('probs: {}'.format(probs))
 
-
-    # compile the circuit down to low-level QASM instructions
-    # supported by the backend (not needed for simple circuits)
+    # Compile the circuit down to low-level QASM instructions
+    # supported by the backend
     compiled_circuit = transpile(circuit, simulator)
 
     # Execute the circuit on the qasm simulator
-    job = simulator.run(compiled_circuit, shots=100)
+    job = simulator.run(compiled_circuit, shots=1000)
 
     # Grab results from the job
-    result = job.result()
-
-    # Returns counts
-    # counts = result.get_counts(compiled_circuit)
-    # print("\nResult:",counts)
+    results = job.result()
 
     # Draw the circuit
     circuit.draw()
+
 
 if __name__ == '__main__':
     main()
