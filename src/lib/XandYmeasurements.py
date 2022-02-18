bb7f10012dc1f3ddd7be5a314b5350b8ab4c9b61
Alex Gil Moreno <30440779+xelivy@users.noreply.github.com>
2022-02-18 09:33:23 +0100

    XandYmeasurement
    Functions to measure in X and Y basis in a single qubit

---
+++ src/lib/XandYmeasurements.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 09:30:04 2022
+
+@author: xelivy
+"""
+
+# Create the X-measurement function:
+def x_measurement(qc, qubit, cbit):
+    """Measure 'qubit' in the X-basis, and store the result in 'cbit'"""
+    qc.h(qubit)
+    qc.measure(qubit, cbit)
+    return qc
+
+# Create the Y-measurement function:
+def y_measurement(qc, qubit, cbit):
+    """Measure 'qubit' in the X-basis, and store the result in 'cbit'"""
+    qc.sdg(qubit)
+    qc.h(qubit)
+    qc.measure(qubit, cbit)
+    return qc
