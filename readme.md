53c4d3bfa78a9a7eb526249863d511ddc709b67e
Fernando Rueda <fernando.rueda@hexad.de>
2022-02-19 21:26:00 +0100

    refactor wState module

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Quantum tomography
+
+## How to install and create virtual environment
+[Virtual environments reference](https://virtualenv.pypa.io/en/latest/)
+```shell
+virtualenv venv
+```
+
+## How to install dependencies
+```shell
+pip install -r requirements.txt
+```
+
+## How to run the code
+```shell
+python3 index.py
+```
+
+#### Other possible issues
+#### Installation for mac M1
+```shell
+pip install "git+https://github.com/Qiskit/qiskit-aer.git"
+```
--- src/lib/wState.py
+++ src/lib/wState.py
@@ -1,26 +1,76 @@
-def buildWState(circuit, referencePosition: int, n: int):
-    if (n < 2):
+import math as m
+import numpy as np
+from qiskit import QuantumCircuit, transpile
+from qiskit.providers.aer import QasmSimulator
+from qiskit.quantum_info import Statevector
+
+
+def w_state(n: int):
+    circuit = QuantumCircuit(n, n)
+
+    if n < 0:
         raise Exception('The size must be at least two.')
-    if (referencePosition < 0):
-        raise Exception('The reference position must be at least zero.')
-    if (n > circuit.width() / 2):
-        raise Exception('The circuit does not have enough qubits.')
-    if (referencePosition + n > circuit.width() / 2):
-        raise Exception('The reference position must be lower.')
-    firstPosition = referencePosition
-    lastPosition = referencePosition + n - 1
-    _circuit = circuit.copy()
-    for i in range(firstPosition, lastPosition + 1):
-        _circuit.reset(i)
-    if (n >= 3):
-        _circuit.ry(2 * np.arccos(1 / m.sqrt(n)), firstPosition)
-        for i in range(n - 3):
-            _circuit.cry(2 * np.arccos(1 / m.sqrt(n - (i + 1))), firstPosition + i, firstPosition + i + 1)
-    if (n == 2):
-        _circuit.h(firstPosition)
+    if n == 2:
+        circuit.h(0)
     else:
-        _circuit.ch(lastPosition - 2, lastPosition - 1)
-    for i in reversed(range(firstPosition, lastPosition)):
-        _circuit.cx(i, i + 1)
-    _circuit.x(firstPosition)
-    return _circuit
+        circuit.ry(2 * np.arccos(1 / m.sqrt(n)), 0)
+
+        for i in range(1, n - 2):
+            circuit.cry(2 * np.arccos(1 / m.sqrt(n - i)), i - 1, i)
+
+        circuit.ch(n - 3, n - 2)
+
+    for i in range(n - 1, 0, -1):
+        circuit.cx(i - 1, i)
+
+    circuit.x(0)
+    return circuit
+
+
+def compose_w_state(qc, q_reference, q_size):
+    if q_reference < 0:
+        raise Exception('The reference position must be at least zero.')
+    if q_reference + q_size > qc.num_qubits:
+        raise Exception('The circuit does not have enough qubits.')
+    wqc = w_state(q_size)
+    return qc.compose(wqc, qubits=range(q_reference, q_reference + q_size))
+
+
+def measure(qc):
+    for i in range(1, qc.num_qubits):
+        qc.measure(i, i)
+
+
+def testWState():
+    simulator = QasmSimulator()
+
+    qc_size = 4
+    w_state_initial_qubit_position = 1
+    w_state_size = 3
+    composed_qc = compose_w_state(QuantumCircuit(qc_size, qc_size), w_state_initial_qubit_position, w_state_size)
+
+    psi = Statevector.from_instruction(composed_qc)
+    probs = psi.probabilities_dict()
+
+    measure(composed_qc)
+
+    compiled_circuit = transpile(composed_qc, simulator)
+    shots = 10
+    job = simulator.run(compiled_circuit, shots=shots)
+
+    counts = job.result().get_counts()
+
+    print(composed_qc)
+    print(counts)
+    print('probs: {}'.format(probs))
+    assert len(probs) == w_state_size
+    offset = 0.02
+    upper_bound = (shots / w_state_size / shots) + offset
+    lower_bound = (shots / w_state_size / shots) - offset
+    for key, value in probs.items():
+        assert value < upper_bound
+        assert value > lower_bound
+
+
+if __name__ == '__main__':
+    testWState()
