737533a44eecd67278025b63a861b6d43633cad7
Gabriel Cuenca <gcuencamoncin@gmail.com>
2022-03-22 17:08:09 +0100

    :bug: fixed dataset_filename path

--- src/lib/RBM_main.py
+++ src/lib/RBM_main.py
@@ -8,22 +8,21 @@ hidden_layers = 3
 learning_rate = 0.1
 epochs = 100
 batch_size = 10
-k = 1 #Number of iterations of Gibbs Sampling.
-dataset_filename = 'training.npy'
+k = 1  # Number of iterations of Gibbs Sampling.
+dataset_filename = 'src/lib/training.npy'
 
 dataset = np.load(dataset_filename)
 
 
+# Creates the network.
+rbm = RBM(visible_layers, hidden_layers)
 
-#Creates the network.
-rbm = RBM(visible_layers,hidden_layers)
+# Trains the network.
+t = time.time()
+rbm.CD_K(dataset, epochs, batch_size, k, learning_rate)
+print('Elapsed time:', time.time() - t)
 
-#Trains the network.
-t=time.time()
-rbm.CD_K(dataset,epochs,batch_size,k,learning_rate)
-print('Elapsed time:',time.time()-t)
-
-#Runs the network. This reconstructs the input sample.
-newData = [0,0,0]
-reconstruction = rbm.run(newData,k)
-print('Reconstruction of the new data:',reconstruction)
+# Runs the network. This reconstructs the input sample.
+newData = [0, 0, 0]
+reconstruction = rbm.run(newData, k)
+print('Reconstruction of the new data:', reconstruction)
