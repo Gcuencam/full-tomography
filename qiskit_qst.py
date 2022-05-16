9794daacc61babaebf6f2862a78066d8db69046d
Fernando Rueda <fernando.rueda@hexad.de>
2022-05-16 08:27:38 +0200

    add output folder to qiskit_qst

--- qiskit_qst.py
+++ qiskit_qst.py
@@ -18,24 +18,24 @@ class ChartType(Enum):
     Time = 'time'
 
 
-def qiskit_tomography(type, qc_size, shots):
-    qc = build(type, QuantumCircuit(qc_size, qc_size), 0, qc_size)
+def qiskit_tomography(experiment_state, qc_size, shots):
+    qc = build(experiment_state, QuantumCircuit(qc_size, qc_size), 0, qc_size)
     # QST Experiment
     qstexp1 = StateTomography(qc)
     qstdata1 = qstexp1.run(AerSimulator(), seed_simulation=100, shots=shots).block_for_results()
 
     state_result = qstdata1.analysis_results("state")
     density_matrix = state_result.value
-    return density_matrix
+    return np.array(density_matrix)
 
 
-def pauli_tomography(type, qc_size, shots):
+def pauli_tomography(experiment_state, qc_size, shots):
     qc = QuantumCircuit(qc_size, qc_size)
     basis = pauli.getCartesianPauliBasis(qc_size)
     ls_measurements = []
 
     for measurement_schema in basis:
-        w_qc = build(type, qc, 0, qc_size)
+        w_qc = build(experiment_state, qc, 0, qc_size)
         pauli.measure_pauli(w_qc, measurement_schema)
         job = simulate(w_qc, shots)
         counts = job.result().get_counts()
@@ -48,9 +48,10 @@ def pauli_tomography(type, qc_size, shots):
     rho_ls = pauli.least_square_estimator(ls_measurements)
     return rho_ls
 
-def sic_tomography(type, qc_size, shots):
+
+def sic_tomography(experiment_state, qc_size, shots):
     qc = QuantumCircuit(qc_size, qc_size)
-    qc = build(type, qc, 0, qc_size)
+    qc = build(experiment_state, qc, 0, qc_size)
     measured_circuit = sic_povm.measure_povm(qc)
 
     job = simulate(measured_circuit, shots)
@@ -62,7 +63,7 @@ def sic_tomography(type, qc_size, shots):
     return rho_ls
 
 
-def append_result(results, experiment_state, qc_size, state_overlaps, time_profile, ):
+def append_result(results, experiment_state, qc_size, state_overlaps, time_profile):
     if experiment_state in results:
         results[experiment_state][qc_size] = {
             ChartType.Overlaps: state_overlaps,
@@ -77,7 +78,7 @@ def append_result(results, experiment_state, qc_size, state_overlaps, time_profi
         }
 
 
-def generate_charts(results, chart_type):
+def generate_charts(results, chart_type, tomography_type, output_folder):
     for i, state_key in enumerate(results):
         state = results[state_key]
         legend = []
@@ -86,68 +87,67 @@ def generate_charts(results, chart_type):
             x_axis = overlaps_by_qubit_size.keys()
             y_axis = overlaps_by_qubit_size.values()
             plt.plot(x_axis, y_axis)
-            legend.append('%s qubits' % qubit_size)
+            legend.append('n = %s' % qubit_size)
 
-        title = state_key
         plt.legend(legend)
         plt.xlabel("N")
-        plt.ylabel(chart_type)
-        # plt.title(title)
-        plt.savefig('%s_chart_%s.png' % (chart_type, title))
+        if chart_type == ChartType.Overlaps:
+            plt.ylabel("O")
+        if chart_type == ChartType.Time:
+            plt.ylabel("T")
+        # plt.title(state_key)
+        plt.savefig('%s/%s/%s_%s.png' % (output_folder, chart_type.value, state_key.value, tomography_type))
         plt.clf()
 
 
-def perform_state_tomography(type, qc_size, shots, tomography_type):
+def perform_state_tomography(experiment_state, qc_size, shots, tomography_type):
     density_matrix = []
     if tomography_type == 'qiskit':
-        density_matrix = qiskit_tomography(type, qc_size, shots)
+        density_matrix = qiskit_tomography(experiment_state, qc_size, shots)
     if tomography_type == 'pauli':
-        density_matrix = pauli_tomography(type, qc_size, shots)
+        density_matrix = pauli_tomography(experiment_state, qc_size, shots)
     if tomography_type == 'sic':
-        density_matrix = sic_tomography(type, qc_size, shots)
+        density_matrix = sic_tomography(experiment_state, qc_size, shots)
 
-    state_vector = get_state_vector(type, qc_size)
+    state_vector = get_state_vector(experiment_state, qc_size)
     overlap = np.sqrt(np.dot(state_vector, np.dot(density_matrix, state_vector.T)))
     print(overlap)
     return overlap
     # print('probs: {}'.format(density_matrix.probabilities()))
 
 
-def experiment(q_bits_range, shots_range, tomography_type):
+def experiment(experiment_state, q_bits_range, shots_range, tomography_type, output_folder):
     results = {}
     for qc_size in q_bits_range:
-        w_state_overlaps = {}
-        w_state_time_profile = {}
-        ghz_state_overlaps = {}
-        ghz_state_time_profile = {}
+        state_overlaps = {}
+        state_time_profile = {}
         for shots in shots_range:
-            print('Performing %s experiment with %s qubits and %s shots' % (States.W, qc_size, shots))
+            print('Performing %s %s experiment with %s qubits and %s shots' % (tomography_type, experiment_state.value, qc_size, shots))
             ts = time.time()
-            w_state_overlaps[shots] = perform_state_tomography(States.W, qc_size, shots, tomography_type)
-            w_state_time_profile[shots] = time.time() - ts
+            state_overlaps[shots] = perform_state_tomography(experiment_state, qc_size, shots, tomography_type)
+            state_time_profile[shots] = time.time() - ts
+            print('Time taken: %s in seconds' % state_time_profile[shots])
 
-            ts = time.time()
-            ghz_state_overlaps[shots] = perform_state_tomography(States.GHZ, qc_size, shots, tomography_type)
-            ghz_state_time_profile[shots] = time.time() - ts
+        append_result(results, experiment_state, qc_size, state_overlaps, state_time_profile)
 
-        append_result(results, States.W, qc_size, w_state_overlaps, w_state_time_profile)
-        append_result(results, States.GHZ, qc_size, ghz_state_overlaps, ghz_state_time_profile)
-
-    generate_charts(results, ChartType.Overlaps)
-    generate_charts(results, ChartType.Time)
+    generate_charts(results, ChartType.Overlaps, tomography_type, output_folder)
+    generate_charts(results, ChartType.Time, tomography_type, output_folder)
 
 
 def main(argv):
-    q_bits_lower_limit = 0
+    q_bits_lower_limit = 2
     q_bits_upper_limit = 6
     shots_lower_limit = 10
     shots_upper_limit = 100
     shots_pace = 10
     tomography_type = 'qiskit'
-    p_help = 'qiskit_qst.py --q_bits_lower_limit=<q_bits_lower_limit> --q_bits_upper_limit=<q_bits_upper_limit> --shots_lower_limit=<shots_lower_limit> --shots_upper_limit=<shots_upper_limit> --shots_pace=<shots_pace> --tomography_type=<tomography_type>'
+    output_folder = './'
+
+    p_help = 'qiskit_qst.py --q_bits_lower_limit=<q_bits_lower_limit> --q_bits_upper_limit=<q_bits_upper_limit> --shots_lower_limit=<shots_lower_limit> --shots_upper_limit=<shots_upper_limit> --shots_pace=<shots_pace> --tomography_type=<tomography_type> --output_folder=<output_folder>'
 
     try:
-        opts, args = getopt.getopt(argv, "", ["q_bits_lower_limit=", "q_bits_upper_limit=", "shots_lower_limit=", "shots_upper_limit=", "shots_pace=", "tomography_type="])
+        opts, args = getopt.getopt(argv, "", ["q_bits_lower_limit=", "q_bits_upper_limit=", "shots_lower_limit=",
+                                              "shots_upper_limit=", "shots_pace=", "tomography_type=", "output_folder="])
     except getopt.GetoptError:
         print(p_help)
         sys.exit(2)
@@ -168,11 +168,21 @@ def main(argv):
             shots_pace = int(arg)
         elif opt == "--tomography_type":
             tomography_type = arg
+        elif opt == "--output_folder":
+            output_folder = arg
 
     q_bits_range = range(q_bits_lower_limit, q_bits_upper_limit + 1)
     shots_range = range(shots_lower_limit, shots_upper_limit + shots_pace, shots_pace)
 
-    experiment(q_bits_range, shots_range, tomography_type)
+    experiment(States.W, q_bits_range, shots_range, 'qiskit', output_folder)
+    experiment(States.GHZ, q_bits_range, shots_range, 'qiskit', output_folder)
+    # experiment(States.Plus, q_bits_range, shots_range, 'qiskit', output_folder)
+    # experiment(States.W, q_bits_range, shots_range, 'pauli', output_folder)
+    # experiment(States.GHZ, q_bits_range, shots_range, 'pauli', output_folder)
+    # experiment(States.Plus, q_bits_range, shots_range, 'pauli', output_folder)
+    # experiment(States.W, q_bits_range, shots_range, 'sic', output_folder)
+    # experiment(States.GHZ, q_bits_range, shots_range, 'sic', output_folder)
+    # experiment(States.Plus, q_bits_range, shots_range, 'sic', output_folder)
 
 
 if __name__ == '__main__':
